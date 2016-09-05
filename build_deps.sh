fa3704893df78fd034161d69adb2f1a2a7359918
xiongfei <fei-04053@163.com>
2016-09-06 01:18:06 +0800

    添加安装 libevent 和 hiredis 的shell程序

--- build_deps.sh
+++ build_deps.sh
@@ -9,6 +9,28 @@ cd $target_dir
 tmp_dist="/home/xiongfei/.lib/dist"
 cpu_cores=4
 
+# ------ build libevent ---------
+lib_dir_name="libevent-2.0.22"
+lib_download_name="libevent-2.0.22-stable.tar.gz"
+if [ ! -d $lib_dir_name ]; then
+    echo "build libevent start"
+    wget "https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz"
+    tar xzvf $lib_download_name
+    mv "libevent-2.0.22-stable" $lib_dir_name
+    if [ -d $tmp_dist ]; then 
+        rm -rf $tmp_dist/*
+    fi
+    cd $lib_dir_name
+    ./configure --prefix=$tmp_dist --enable-shared=false
+    make -j$cpu_cores
+    make install
+    cd ..
+    rm -rf $lib_download_name
+    rm -rf $lib_dir_name
+    mv $tmp_dist $lib_dir_name
+    echo "build libevent end"
+fi
+
 # ------ buid libevhtp -----------
 lib_dir_name="libevhtp-1.2.10"
 lib_download_name="1.2.10.tar.gz"
@@ -31,7 +53,6 @@ if [ ! -d $lib_dir_name ]; then
     echo "build libevhtp complete"
 fi
 
-
 # -------- build libconfig ----------
 lib_download_name="v1.5.tar.gz"
 lib_dir_name="libconfig-1.5"
@@ -39,6 +60,9 @@ if [ ! -d $lib_dir_name ]; then
     echo "build libconfig start"
     wget 'https://github.com/hyperrealm/libconfig/archive/v1.5.tar.gz'
     tar xzvf $lib_download_name
+    if [ -d $tmp_dist ]; then 
+        rm -rf $tmp_dist/*
+    fi
     cd $lib_dir_name
     aclocal
     autoconf
@@ -53,4 +77,23 @@ if [ ! -d $lib_dir_name ]; then
     echo "build libconfig end"
 fi
 
-# --------- build xxxxxxx------------
+# --------- build hiredis  ------------
+lib_download_name="v0.13.3.tar.gz"
+lib_dir_name="hiredis-0.13.3"
+if [ ! -d $lib_dir_name ]; then
+    echo "build hiredis start"
+    wget "https://github.com/redis/hiredis/archive/v0.13.3.tar.gz"
+    tar xzvf $lib_download_name
+    if [ -d $tmp_dist ]; then 
+        rm -rf $tmp_dist/*
+    fi
+    cd $lib_dir_name
+    make -j$cpu_cores
+    make install PREFIX=$tmp_dist
+    cd ..
+    rm $tmp_dist/lib/libhiredis.so*
+    rm -rf $lib_download_name
+    rm -rf $lib_dir_name
+    mv $tmp_dist $lib_dir_name
+    echo "build hiredis end"
+fi
